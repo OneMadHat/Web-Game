9d7dad90c40781bace49e21c875d02846db14700
OneMadHat <dlsdragonforce@gmail.com>
2016-11-02 11:57:09 -0500

    Test

--- js/Game.js
+++ js/Game.js
@@ -1,13 +1,56 @@
-<script type="text/javascript">
-    function changeImage(a) {
-        document.getElementById("img").src=a;
-    }
-</script>
-<div id="main_img">
-    <img id="img" src="http://placehold.it/110x110">
-</div>
-<div id="thumb_img">
-    <img src='http://placehold.it/120x60'  onclick='changeImage("http://placehold.it/120x60");'>
-    <img src='http://placehold.it/130x60'  onclick='changeImage("http://placehold.it/130x60");'>
-    <img src='http://placehold.it/140x60'  onclick='changeImage("http://placehold.it/140x60");'>
-</div>
+function GS(x){
+
+document.getElementById('img').innerHTML = "";
+document.getElementById('img').innerHTML += '<script>';
+document.getElementById('img').innerHTML += '<img src="pictures/Hacked.jpg" alt="img" />';
+document.getElementById('img').innerHTML += '</script>';
+
+switch(x) {
+
+	case 1:
+		GSL = "pictures/start.jpg"
+		document.getElementById('img').innerHTML = "";
+		document.getElementById('img').innerHTML += '<script>';
+		document.getElementById('img').innerHTML += '<img src="pictures/start.jpg" alt="img" />';
+		document.getElementById('img').innerHTML += '</script>';
+		break;
+
+	case 2:
+
+	case 3:
+
+	case 4:
+
+	case 5:
+
+	case 6:
+
+	case 7:
+
+	case 8:
+
+	case 9:
+
+	case 10:
+
+	case 11:
+
+	case 12:
+
+	case 13:
+
+	case 14:
+
+	case 15:
+
+	case 16:
+
+	case 17:
+
+	case 18:
+
+	case 19:
+
+	case 20:
+}
+}
