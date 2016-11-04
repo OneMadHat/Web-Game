44344e00595d9e6edc796b8482638aed42dbb4d5
OneMadHat <dlsdragonforce@gmail.com>
2016-11-03 21:29:39 -0500

    I AM COMMITED

--- js/Counter.js
+++ js/Counter.js
@@ -2,25 +2,36 @@
 * Basic Count Up from Date and Time
 * Author: @mrwigster / trulycode.com
 */
+test = 1;
 window.onload=function() {
 
   upTime(new Date());
 }
 function upTime(countTo) {
-  now = new Date();
-  countTo = new Date(countTo);
-  difference = (now-countTo);
+  if(test <= 1){
+    now = new Date();
+    countTo = new Date(countTo);
+    difference = (now-countTo);
 
-  days=Math.floor(difference/(60*60*1000*24)*1);
-  hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
-  mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
-  secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
+    hours=Math.floor((difference%(60*60*1000*24))/(60*60*1000)*1);
+    mins=Math.floor(((difference%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+    secs=Math.floor((((difference%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
 
-  document.getElementById('days').firstChild.nodeValue = days;
-  document.getElementById('hours').firstChild.nodeValue = hours;
-  document.getElementById('minutes').firstChild.nodeValue = mins;
-  document.getElementById('seconds').firstChild.nodeValue = secs;
+    document.getElementById('hours').firstChild.nodeValue = hours;
+    document.getElementById('minutes').firstChild.nodeValue = mins;
+    document.getElementById('seconds').firstChild.nodeValue = secs;
 
-  clearTimeout(upTime.to);
-  upTime.to=setTimeout(function(){ upTime(countTo); },1000);
+    clearTimeout(upTime.to);
+    upTime.to=setTimeout(function(){ upTime(countTo); },1000);
+  /* Tells the user the took to long and lost */  
+   /* if(secs == 10){
+        document.getElementById('img').innerHTML = "";
+        document.getElementById('img').innerHTML += '<script>';
+        document.getElementById('img').innerHTML += '<img src="pictures/win_not.jpg" alt="img" />';
+        document.getElementById('img').innerHTML += '</script>';
+        clearInterval(upTime);
+        test = 2;
+        
+    }*/
+  }
 }
--- js/Game.js
+++ js/Game.js
@@ -1,32 +1,45 @@
-function GS(x){
-
-switch(x) {
-
-	case 1:
+function GS(x, z, y){
+/* Sarting Game Screen */
+switch(y) {
+	case 'start':
 		document.getElementById('img').innerHTML = "";
 		document.getElementById('img').innerHTML += '<script>';
 		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS1.jpg" alt="img" />';
 		document.getElementById('img').innerHTML += '</script>';
+		/* button change */
 		document.getElementById('button').innerHTML = "";
+		/* story cahnge */
+		document.getElementById('story').innerHTML = '<p>This is a test to see if it works correcly</p>'
+		/* iquit change */
+		document.getElementById('iquit').innerHTML += '<button type="submit"> I QUIT </button>'
+		/* GSbutton change */
 		document.getElementById('GSbutton').innerHTML = "";
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use Terminal" onclick="GS(0, 1);" />';
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use WireShark" onclick="GS(1, 0);" />';
+}
 
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use Terminal" onclick="GS(2);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use WireShark" onclick="GS(3);" />';
+/* Wire Shark Game Screen */
+switch(x) {
+	case 0:
 		break;
 
-	case 2:
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS2.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
-		break;
-
-	case 3:
+	case 1:
+		/* WireShark Game Screen 1 */
 		document.getElementById('img').innerHTML = "";
 		document.getElementById('img').innerHTML += '<script>';
 		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS3.jpg" alt="img" />';
 		document.getElementById('img').innerHTML += '</script>';
+
+		document.getElementById('GSbutton').innerHTML = "";
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';
 		break;
+
+	case 2:
+
+		break;
+
+	case 3:
 
 	case 4:
 
@@ -62,4 +75,46 @@ switch(x) {
 
 	case 20:
 }
+
+/* Terminal Game Screen */
+switch(z) {
+	case 0:
+		break;
+
+	case 1:
+		/* Terminal Game Screen 1 */
+		document.getElementById('img').innerHTML = "";
+		document.getElementById('img').innerHTML += '<script>';
+		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS2.jpg" alt="img" />';
+		document.getElementById('img').innerHTML += '</script>';
+
+		document.getElementById('GSbutton').innerHTML = "";
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Type in ifconfig" onclick="GS(0, 2);" />';
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Ping google.com" onclick="GS(0, 3);" />';
+		break;
+
+	case 2:
+		document.getElementById('img').innerHTML = "";
+		document.getElementById('img').innerHTML += '<script>';
+		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS4.jpg" alt="img" />';
+		document.getElementById('img').innerHTML += '</script>';
+
+		document.getElementById('GSbutton').innerHTML = "";
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';		
+		break;
+
+	case 3:
+		document.getElementById('img').innerHTML = "";
+		document.getElementById('img').innerHTML += '<script>';
+		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS5.jpg" alt="img" />';
+		document.getElementById('img').innerHTML += '</script>';
+
+		document.getElementById('GSbutton').innerHTML = "";
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';
+		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';		
+	break;
+
+}
+
 }
