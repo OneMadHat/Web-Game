e16d4ff4eed751b18d28cdfea8abc3eaa2b22f67
OneMadHat <dlsdragonforce@gmail.com>
2016-11-17 20:12:59 -0600

    More shit donw

--- js/Game.js
+++ js/Game.js
@@ -1,41 +1,44 @@
 function GS(x, y, z){
-
+var Dimg = document.getElementById('img');
+var Dstory = document.getElementById('story');
+var Diquit = document.getElementById('iquit');
+var DGSbutton = document.getElementById('GSbutton');
 
 /* Sarting Game Screen */
 if (z=="start") {
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS1.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS1.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* button change */
 		document.getElementById('button').innerHTML = "";
 		/* story cahnge */
-		document.getElementById('story').innerHTML = '<p>You log into Kali to figure out what the Hacker took and inspect deeper. You can use the Terminal to get current system info. or you can use Wireshark to see all incoming and outgoing packets. Choose wisely as you are on the clock.</p>'
+		Dstory.innerHTML = '<p>You log into Kali to figure out what the Hacker took and inspect deeper. You can use the Terminal to get current system info. or you can use Wireshark to see all incoming and outgoing packets. Choose wisely as you are on the clock.</p>'
 		/* iquit change */
-		document.getElementById('iquit').innerHTML += '<button type="submit"> I QUIT </button>'
+		Diquit.innerHTML += '<button type="submit"> I QUIT </button>'
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use Terminal" onclick="GS(0, 1);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use WireShark" onclick="GS(1, 0);" />';
+		DGSbutton.innerHTML = "";
+		DGSbutton.innerHTML += '<input type="button" value="Use Terminal" onclick="GS(0, 1);" />';
+		DGSbutton.innerHTML += '<input type="button" value="Use WireShark" onclick="GS(1, 0);" />';
 }
 
-/* Wire Shark Game Screen */
+/* WireShark Game Screen Start ▼ */
 switch(x) {
 	case 0:
 		break;
 
 	case 1:
 		/* WireShark Game Screen 1 */
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS1W.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS1W.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "You open Wireshark and you come to a revelation. The attack has already happened. You need to see who they are, what they took and where are they from.";
+		Dstory.innerHTML = "You open Wireshark and you come to a revelation. The attack has already happened. You need to see who they are, what they took and where are they from.";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Use Something that works" onclick="GS(0,1);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="WE ARE BEING SERIOUS!!!" onclick="GS(0,1);" />';
+		DGSbutton.innerHTML = "";
+		DGSbutton.innerHTML += '<input type="button" value="Use Something that works" onclick="GS(0,1);" />';
+		DGSbutton.innerHTML += '<input type="button" value="WE ARE BEING SERIOUS!!!" onclick="GS(0,1);" />';
 
 		break;
 
@@ -43,78 +46,79 @@ switch(x) {
 
 		break;
 }
+/*WireShark Game Screen End ▲ */
 
-/* Terminal Game Screen */
+/* Terminal Game Screen Start ▼*/
 switch(y) {
 	case 0:
 		break;
 
 	case 1:
-		/* Terminal Game Screen 1 */
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS1T.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS1T.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "You open a Terminal and it is time to view those logs and see who was here or you can Ping Google to make sure you are still on-line.";
+		Dstory.innerHTML = "You open a Terminal and it is time to view those logs and see who was here or you can Ping Google to make sure you are still on-line.";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="View System logs" onclick="GS(0, 2);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="Ping google.com" onclick="GS(0, 3);" />';
+		DGSbutton.innerHTML = "";
+		DGSbutton.innerHTML += '<input type="button" value="View System logs" onclick="GS(0, 2);" />';
+		DGSbutton.innerHTML += '<input type="button" value="Ping google.com" onclick="GS(0, 3);" />';
 		break;
 
 	case 2:
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS2T.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS2T.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "You find an unknown individual in the logs. You can try and run TraceRoute on them to see where they are from or you can try and hack them to return the favor. There is always the smarter option. ";
+		Dstory.innerHTML = "You find an unknown individual in the logs. You can try and run TraceRoute on them to see where they are from or you can try and hack them to return the favor. There is always the smarter option. ";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="TraceRoute" onclick="GS(0, 4);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="HACK THEM!!" onclick="GS(0, 5);" />';		
+		DGSbutton.innerHTML = "";
+		DGSbutton.innerHTML += '<input type="button" value="TraceRoute" onclick="GS(0, 4);" />';
+		DGSbutton.innerHTML += '<input type="button" value="HACK THEM!!" onclick="GS(0, 5);" />';		
 		break;
 
 	case 3:
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS3T.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS3T.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "???";
+		Dstory.innerHTML = "???";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="View System logs" onclick="GS(0, 2);" />';
-		document.getElementById('GSbutton').innerHTML += '<input type="button" value="???" onclick="GS(???);" />';		
+		DGSbutton.innerHTML = "";
+		DGSbutton.innerHTML += '<input type="button" value="View System logs" onclick="GS(0, 2);" />';
+		DGSbutton.innerHTML += '<input type="button" value="???" onclick="GS(???);" />';		
 	break;
 
 	case 4:
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS45T.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS45T.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "You launch Tracerout to locate where they are from and that happened to be a poor decision.";
+		Dstory.innerHTML = "You launch Tracerout to locate where they are from and that happened to be a poor decision.";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
+		DGSbutton.innerHTML = "";
 		/* iquit change */
-		document.getElementById('iquit').innerHTML = ''
+		Diquit.innerHTML = ''
 	break;
 
 	case 5:
-		document.getElementById('img').innerHTML = "";
-		document.getElementById('img').innerHTML += '<script>';
-		document.getElementById('img').innerHTML += '<img src="pictures/Game Screens/GS45T.jpg" alt="img" />';
-		document.getElementById('img').innerHTML += '</script>';
+		Dimg.innerHTML = "";
+		Dimg.innerHTML += '<script>';
+		Dimg.innerHTML += '<img src="pictures/Game Screens/GS45T.jpg" alt="img" />';
+		Dimg.innerHTML += '</script>';
 		/* Story board */
-		document.getElementById('story').innerHTML = "You try to hack them and that chose to be a poor decision.";
+		Dstory.innerHTML = "You try to hack them and that chose to be a poor decision.";
 		/* GSbutton change */
-		document.getElementById('GSbutton').innerHTML = "";
+		DGSbutton.innerHTML = "";
 		/* iquit change */
-		document.getElementById('iquit').innerHTML = ''
+		Diquit.innerHTML = ''
 	break;
 
 }
+/* Terminal Game Screen End ▲ */
 
 }
